c8ced8a9205cad2f13dba17196a353098bbc1329
vardhan0604 <harsh062004@gmail.com>
2023-07-21 23:10:52 +0530

    Added login component and cleared the excess code

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <form>
+        <input type="text" />
+        <input type="password" />
+        <button>Login</button>
+    </form>
+  )
+}
+
+export default Login
